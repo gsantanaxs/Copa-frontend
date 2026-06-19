afda089e426b81c252508fe9055cd5b93bbe58b4
Giovanna 🌸 <giovanna.s.almeida8@aluno.senai.br>
2026-06-19 08:57:17 -0300

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://copa-backend-2.vercel.app/api' // URL do backend publicado na Vercel (rotas montadas sob /api)
+const API_URL = 'https://copa-backend-2.vercel.app/api' // URL do backend publicado na Vercel
 
 class API {
     constructor() {
