7c0c9fc39a50d9d2452d948b14b1d72a3c868e93
Guilherme Constantino <guilherme.constantino.1b@gmail.com>
2026-06-19 07:40:53 -0300

    Update API_URL and refactor API class

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://copa-backend-2.vercel.app/api' // URL do backend publicado na Vercel
+const API_URL = 'https://copa-backend-2.vercel.app/' // URL do backend publicado na Vercel
 
 class API {
     constructor() {
